385ae02114a5e14b6af0b0ef3161bd48e452a90b
unknown <Monirul-Jim>
2023-12-05 17:55:08 +0600

    added admin seller role

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -13,12 +14,28 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
+const jsonWebToken = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dsd2lyy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,6 +55,22 @@ async function run() {
         const productCollection = client.db('MediaAid').collection('productCollection');
         const categoryCollection = client.db('MediaAid').collection('categoryCollection');
         const userOrderCollection = client.db('MediaAid').collection('userOrderCollection');
+        const AllUserCollection = client.db("MediaAid").collection("all-userCollection");
+        // create jwt token to secure api
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '30d' })
+            res.send({ token })
+        })
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await AllUserCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
         app.post('/increase-category', async (req, res) => {
             const orderData = req.body;
             const result = await categoryCollection.insertOne(orderData);
@@ -155,20 +188,79 @@ async function run() {
             const { id } = req.params;
             try {
                 const result = await userOrderCollection.findOne({ _id: new ObjectId(id) });
-
                 if (!result) {
                     return res.status(404).send('User order not found');
                 }
-
                 res.send(result);
             } catch (error) {
                 console.error('Error fetching user order:', error);
                 res.status(500).send('Internal Server Error');
             }
         });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email };
+            const alreadyExistUser = await AllUserCollection.findOne(query)
+            if (alreadyExistUser) {
+                return res.send({ message: 'user already in the database' })
+            }
+            const result = await AllUserCollection.insertOne(user)
+            res.send(result)
+        })
+        app.get('/users', async (req, res) => {
+            const result = await AllUserCollection.find().toArray();
+            res.send(result);
+        });
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
 
+            const result = await AllUserCollection.updateOne(filter, updateDoc);
+            res.send(result);
 
-
+        })
+        app.get('/users/admin/:email', jsonWebToken, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return res.send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await AllUserCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+        app.patch('/users/seller/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'seller'
+                },
+            };
+            const result = await AllUserCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+        app.get('/users/seller/:email', jsonWebToken, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return res.send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await AllUserCollection.findOne(query);
+            const result = { admin: user?.role === 'seller' }
+            res.send(result);
+        })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await AllUserCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
